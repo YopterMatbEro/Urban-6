534b7b99746abe2f526e52dd179e5f930370e310
YopterMatbEro <yopter2013@gmail.com>
2024-09-05 11:53:18 +0300

    How Pegasus was made v1.1 (rework)

--- module_6_3.py
+++ module_6_3.py
@@ -1,25 +1,25 @@
 class Horse:
-    def __init__(self, x_distance: int = 0, sound: str = 'Frrr'):
-        self.x_distance = x_distance
-        self.sound = sound
+    def __init__(self):
+        self.x_distance = 0
+        self.sound = 'Frrr'
 
     def run(self, dx: int):
         self.x_distance += dx
 
 
 class Eagle:
-    def __init__(self, y_distance: int = 0, sound: str = 'I train, eat, sleep, and repeat'):
-        self.y_distance = y_distance
-        self.sound = sound
+    def __init__(self):
+        self.y_distance = 0
+        self.sound = 'I train, eat, sleep, and repeat'
 
     def fly(self, dy: int):
         self.y_distance += dy
 
 
 class Pegasus(Horse, Eagle):
-    def __init__(self, x_distance: int = 0, y_distance: int = 0, sound: str = 'I train, eat, sleep, and repeat'):
-        Horse.__init__(self, x_distance, sound)
-        Eagle.__init__(self, y_distance, sound)
+    def __init__(self):
+        Horse.__init__(self)
+        Eagle.__init__(self)
 
     def move(self, dx: int, dy: int):
         self.run(dx)
